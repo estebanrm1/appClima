b82b952f81915bf69b88527a38aac37bd60cc3a3
estebanrm1 <solsel90@gmail.com>
2023-06-10 12:55:04 -0300

    se agrego los archivos para los componentes, el background y el titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
   return (
     <>
-      
+    <div className='bg-succes bg-opacity-50 bg-primary text-light p-3 text-center'>
+      <h1>App Clima</h1>
+    </div>
     </>
   )
 }
